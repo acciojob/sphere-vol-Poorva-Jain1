29ebd4c41d665a217e211e9995dc6028bea41646
Poorva-Jain1 <poorvajainnagod@gmail.com>
2025-12-09 23:25:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function volume_sphere() {
     //Write your code here
+	let radius = document.getElementById("radius").value;
+	 let volume;
+	if (isNaN(radius) || radius < 0) {
+        volume = NaN;
+    } else {
+		 volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+		volume = volume.toFixed(4);
+    }
+	document.getElementById("volume").value = volume;
+	 return false;
+
   
 } 
 
